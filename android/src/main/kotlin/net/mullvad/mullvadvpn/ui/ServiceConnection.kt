558079dc1170aa62d5188755a7c364b71fb8da69
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-04 21:58:18 +0000

    Register for service events in `ServiceConnection`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ServiceConnection.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ServiceConnection.kt
@@ -1,10 +1,18 @@
 package net.mullvad.mullvadvpn.ui
 
+import android.os.Looper
+import android.os.Messenger
+import android.os.RemoteException
+import android.util.Log
 import net.mullvad.mullvadvpn.dataproxy.AppVersionInfoCache
 import net.mullvad.mullvadvpn.dataproxy.RelayListListener
+import net.mullvad.mullvadvpn.service.Request
 import net.mullvad.mullvadvpn.service.ServiceInstance
+import net.mullvad.mullvadvpn.ui.serviceconnection.EventDispatcher
 
 class ServiceConnection(private val service: ServiceInstance, val mainActivity: MainActivity) {
+    val dispatcher = EventDispatcher(Looper.getMainLooper())
+
     val daemon = service.daemon
     val accountCache = service.accountCache
     val connectionProxy = service.connectionProxy
@@ -20,11 +28,26 @@ class ServiceConnection(private val service: ServiceInstance, val mainActivity: 
     init {
         appVersionInfoCache.onCreate()
         connectionProxy.mainActivity = mainActivity
+        registerListener()
     }
 
     fun onDestroy() {
+        dispatcher.onDestroy()
+
         appVersionInfoCache.onDestroy()
         relayListListener.onDestroy()
         connectionProxy.mainActivity = null
+    }
+
+    private fun registerListener() {
+        val message = Request.RegisterListener().message.apply {
+            replyTo = Messenger(dispatcher)
+        }
+
+        try {
+            service.messenger.send(message)
+        } catch (exception: RemoteException) {
+            Log.e("mullvad", "Failed to register listener for service events", exception)
+        }
     }
 }
