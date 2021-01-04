79e1cab4a8412907531282150140bfedc28e56d4
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-04 18:38:22 +0000

    Send initial split tunneling status to listener

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceHandler.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceHandler.kt
@@ -129,6 +129,7 @@ class ServiceHandler(
             send(Event.SettingsUpdate(settingsListener.settings).message)
             send(Event.NewLocation(locationInfoCache.location).message)
             send(Event.WireGuardKeyStatus(keyStatusListener.keyStatus).message)
+            send(Event.SplitTunnelingUpdate(splitTunneling.onChange.latestEvent).message)
             send(Event.ListenerReady().message)
         }
     }
