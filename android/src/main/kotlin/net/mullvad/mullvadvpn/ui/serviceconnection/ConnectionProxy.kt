f92f61dbcd6006de2d3d376c0c817770e2016396
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-05 00:30:43 +0000

    Create UI-side `ConnectionProxy`

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/ConnectionProxy.kt
@@ -0,0 +1,33 @@
+package net.mullvad.mullvadvpn.ui.serviceconnection
+
+import android.os.Messenger
+import net.mullvad.mullvadvpn.model.TunnelState
+import net.mullvad.mullvadvpn.service.Event
+import net.mullvad.mullvadvpn.service.Request
+import net.mullvad.talpid.util.EventNotifier
+
+class ConnectionProxy(val connection: Messenger, eventDispatcher: EventDispatcher) {
+    val onStateChange = EventNotifier<TunnelState>(TunnelState.Disconnected())
+
+    init {
+        eventDispatcher.registerHandler(Event.TunnelStateChange::class) { event ->
+            onStateChange.notify(event.tunnelState)
+        }
+    }
+
+    fun connect() {
+        connection.send(Request.Connect().message)
+    }
+
+    fun disconnect() {
+        connection.send(Request.Disconnect().message)
+    }
+
+    fun reconnect() {
+        connection.send(Request.Reconnect().message)
+    }
+
+    fun onDestroy() {
+        onStateChange.unsubscribeAll()
+    }
+}
