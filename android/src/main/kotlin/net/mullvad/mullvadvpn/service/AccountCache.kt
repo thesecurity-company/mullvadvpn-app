bd3e338bb5c05e3772e30b6d39a067b2163f9515
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-15 12:46:50 +0000

    Refactor `AccountCache` login logic into an actor

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/AccountCache.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/AccountCache.kt
@@ -1,5 +1,11 @@
 package net.mullvad.mullvadvpn.service
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.channels.sendBlocking
 import kotlinx.coroutines.delay
 import net.mullvad.mullvadvpn.model.GetAccountDataResult
 import net.mullvad.mullvadvpn.model.LoginStatus
@@ -23,7 +29,14 @@ class AccountCache(
         // This is only used if the expiry was invalidated and fetching a new expiry returns the
         // same value as before the invalidation.
         private const val MAX_INVALIDATED_RETRIES = 7
+
+        private sealed class Command {
+            class CreateAccount() : Command()
+            class Login(val account: String) : Command()
+        }
     }
+
+    private val commandChannel = spawnActor()
 
     val onAccountNumberChange = EventNotifier<String?>(null)
     val onAccountExpiryChange = EventNotifier<DateTime?>(null)
@@ -51,18 +64,12 @@ class AccountCache(
         }
     }
 
-    suspend fun createNewAccount() {
-        newlyCreatedAccount = true
-        createdAccountExpiry = null
-
-        daemon.await().createNewAccount()
+    fun createNewAccount() {
+        commandChannel.sendBlocking(Command.CreateAccount())
     }
 
-    suspend fun login(account: String) {
-        if (account != accountNumber) {
-            markAccountAsNotNew()
-            daemon.await().setAccount(account)
-        }
+    fun login(account: String) {
+        commandChannel.sendBlocking(Command.Login(account))
     }
 
     fun fetchAccountExpiry() {
@@ -118,6 +125,35 @@ class AccountCache(
         onAccountExpiryChange.unsubscribeAll()
         onAccountHistoryChange.unsubscribeAll()
         onLoginStatusChange.unsubscribeAll()
+
+        commandChannel.close()
+    }
+
+    private fun spawnActor() = GlobalScope.actor<Command>(Dispatchers.Default, Channel.UNLIMITED) {
+        try {
+            val command = channel.receive()
+
+            when (command) {
+                is Command.CreateAccount -> doCreateAccount()
+                is Command.Login -> doLogin(command.account)
+            }
+        } catch (exception: ClosedReceiveChannelException) {
+            // Command channel was closed, stop the actor
+        }
+    }
+
+    private suspend fun doCreateAccount() {
+        newlyCreatedAccount = true
+        createdAccountExpiry = null
+
+        daemon.await().createNewAccount()
+    }
+
+    private suspend fun doLogin(account: String) {
+        if (account != accountNumber) {
+            markAccountAsNotNew()
+            daemon.await().setAccount(account)
+        }
     }
 
     private fun fetchAccountHistory() {
