3e073071b3cca74869ce87fde3df757a0a37c345
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-26 00:56:01 +0000

    Send an event when listener is registered

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceHandler.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceHandler.kt
@@ -1,5 +1,6 @@
 package net.mullvad.mullvadvpn.service
 
+import android.os.DeadObjectException
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
@@ -18,11 +19,35 @@ class ServiceHandler(
         val request = Request.fromMessage(message)
 
         when (request) {
-            is Request.RegisterListener -> listeners.add(message.replyTo)
+            is Request.RegisterListener -> registerListener(message.replyTo)
         }
     }
 
     fun onDestroy() {
         settingsListener.onDestroy()
+    }
+
+    private fun registerListener(listener: Messenger) {
+        listeners.add(listener)
+
+        listener.apply {
+            send(Event.ListenerReady().message)
+        }
+    }
+
+    private fun sendEvent(event: Event) {
+        val deadListeners = mutableListOf<Messenger>()
+
+        for (listener in listeners) {
+            try {
+                listener.send(event.message)
+            } catch (_: DeadObjectException) {
+                deadListeners.add(listener)
+            }
+        }
+
+        for (deadListener in deadListeners) {
+            listeners.remove(deadListener)
+        }
     }
 }
